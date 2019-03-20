c9affb1c16a21bbc1b15efd662b58c52419494ef
Andrew <920076768@qq.com>
2019-03-20 11:23:45 +0800

    Initial commit

---
+++ android/app/src/main/java/zw526/com/countdown/MainActivity.java
@@ -0,0 +1,13 @@
+package zw526.com.countdown;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
